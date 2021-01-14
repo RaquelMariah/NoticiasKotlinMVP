dad4e18ccf22f426f2dff456186ca21a9fcbeb41
Raquel Maria <raquel.maria@vr.com.br>
2021-01-14 11:21:50 -0300

    Adicionada activity noticia

---
+++ app/src/main/java/com/ramaria/aplicativonoticiaskotlin/ArticleActivity.kt
@@ -0,0 +1,11 @@
+package com.ramaria.aplicativonoticiaskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
