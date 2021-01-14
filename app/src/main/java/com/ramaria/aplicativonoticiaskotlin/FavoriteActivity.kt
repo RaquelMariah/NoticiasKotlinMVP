c709c7dbb91e7453343770ac02e68388b0fc2e67
Raquel Maria <raquel.maria@vr.com.br>
2021-01-14 11:23:43 -0300

    Adicionada activity favoritos

---
+++ app/src/main/java/com/ramaria/aplicativonoticiaskotlin/FavoriteActivity.kt
@@ -0,0 +1,11 @@
+package com.ramaria.aplicativonoticiaskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
