283b934a24d5e1fb99c5e7c8a8f28a09afb43809
Raquel Maria <raquel.maria@vr.com.br>
2021-01-18 18:12:20 -0300

    Configurado floating action button

--- app/src/main/java/com/ramaria/aplicativonoticiaskotlin/ui/ArticleActivity.kt
+++ app/src/main/java/com/ramaria/aplicativonoticiaskotlin/ui/ArticleActivity.kt
@@ -3,8 +3,10 @@ package com.ramaria.aplicativonoticiaskotlin.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebViewClient
+import com.google.android.material.snackbar.Snackbar
 import com.ramaria.aplicativonoticiaskotlin.R
 import com.ramaria.aplicativonoticiaskotlin.model.Article
+import com.ramaria.aplicativonoticiaskotlin.model.database.NewsDataSource
 import kotlinx.android.synthetic.main.activity_article.*
 
 class ArticleActivity : AbstractActivity() {
@@ -16,6 +18,7 @@ class ArticleActivity : AbstractActivity() {
 
     override fun onInject() {
         getArticle()
+        val dataSource = NewsDataSource(this)
 
         webView.apply{
             webViewClient = WebViewClient()
@@ -24,6 +27,10 @@ class ArticleActivity : AbstractActivity() {
             }
         }
 
+        fabArticle.setOnClickListener{
+            dataSource.saveArticle(article)
+            Snackbar.make(it, R.string.article_saved_successful, Snackbar.LENGTH_LONG).show()
+        }
     }
 
     private fun getArticle() {
